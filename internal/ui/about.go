0e0e716fe9547ad9b82ffeb95df7d01a0087db01
bosslawl <boss@rain.ac>
2023-05-21 02:58:26 +0100

    v2.0.3

--- internal/ui/about.go
+++ internal/ui/about.go
@@ -29,7 +29,7 @@ func (a *about) buildUI() *fyne.Container {
 	const (
 		https   = "https"
 		github  = "github.com"
-		version = "v2.0.2"
+		version = "v2.0.3"
 	)
 
 	repoURL := &url.URL{Scheme: https, Host: github, Path: "/bosslawl/HeadshotHider"}
